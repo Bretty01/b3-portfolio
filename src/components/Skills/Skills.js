61151c0208d49f1ff590a34b175e7f7bcceb623b
Brett <brettyboy84@live.com>
2022-11-02 12:18:31 -0600

    Added more icons to Skills.js

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,19 +6,26 @@ import CSS3 from "../Icons/CSS3/CSS3"
 import HTML5 from "../Icons/HTML5/HTML5"
 import Bootstrap from "../Icons/Bootstrap/Bootstrap"
 import JQuery from "../Icons/JQuery/JQuery"
+import MongoDB from "../Icons/MongoDB/MongoDB"
+import NodeJS from "../Icons/NodeJS/NodeJS"
+import REST from "../Icons/REST/REST"
+import PHP from "../Icons/PHP/PHP"
+import GraphQL from "../Icons/GraphQL/GraphQL"
 const Skills = () => {
     const tab1 = [
         {title: "React", image: <React />},
         {title: "Redux", image: <Redux />},
         {title: "CSS3", image: <CSS3 />},
         {title: "HTML5", image: <HTML5 />},
-        {title: "Bootstrap", image: <Bootstrap />}
+        {title: "Bootstrap", image: <Bootstrap />},
+        {title: "JQuery", image: <JQuery />}
     ]
     const tab2 = [
-        {title: "JQuery", image: <JQuery />},
-        {title: "JQuery", image: <JQuery />},
-        {title: "JQuery", image: <JQuery />},
-        {title: "JQuery", image: <JQuery />}
+        {title: "MongoDB", image: <MongoDB />},
+        {title: "NodeJS", image: <NodeJS />},
+        {title: "REST", image: <REST />},
+        {title: "PHP", image: <PHP />},
+        {title: "GraphQL", image: <GraphQL />}
 
     ]
     const tab3 = [
