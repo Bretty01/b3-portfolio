fec01db2831076a8d5d6cf6fa33e40b7881c1064
Brett <brettyboy84@live.com>
2023-01-27 15:44:44 -0600

    Adjustments to Skill icon fill method

--- src/components/Icons/Kotlin/Kotlin.js
+++ src/components/Icons/Kotlin/Kotlin.js
@@ -5,22 +5,22 @@ const Kotlin = () => {
         <g>
 
         <linearGradient id="XMLID_3_" gradientUnits="userSpaceOnUse" x1="15.9594" y1="-13.0143" x2="44.3068" y2="15.3332" gradientTransform="matrix(1 0 0 -1 0 61)">
-            <stop  offset="9.677000e-02" stopColor="#AAAAAA"/>
-            <stop  offset="1" stopColor="#FFFFFF"/>
+            <stop  offset="9.677000e-02" stopColor="#DDD"/>
+            <stop  offset="1" stopColor="var(--basic-light)"/>
         </linearGradient>
-    <polygon id="XMLID_2_" fill="url(#XMLID_3_)" points="0,60 30.1,29.9 60,60 	"/>
+    <polygon id="XMLID_2_" fill="url(#XMLID_3_)" points="0,60 30.1,29.9 60,60" stroke="none"/>
 
     <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="4.2092" y1="48.9409" x2="20.6734" y2="65.405" gradientTransform="matrix(1 0 0 -1 0 61)">
-        <stop  offset="0.1183" stopColor="#AAAAAA"/>
-        <stop  offset="0.8333" stopColor="#FFFFFF"/>
+        <stop  offset="0.1183" stopColor="#DDD"/>
+        <stop  offset="0.8333" stopColor="var(--basic-light)"/>
     </linearGradient>
-    <polygon fill="url(#SVGID_1_)" points="0,0 30.1,0 0,32.5 	"/>
+    <polygon fill="url(#SVGID_1_)" points="0,0 30.1,0 0,32.5 "  stroke="none"/>
 
     <linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="-10.1017" y1="5.8362" x2="45.7315" y2="61.6694" gradientTransform="matrix(1 0 0 -1 0 61)">
-        <stop  offset="0.1075" stopColor="#FFFFFF"/>
-        <stop  offset="0.8232"stopColor="#AAAAAA"/>
+        <stop  offset="0.1075" stopColor="var(--basic-light)"/>
+        <stop  offset="0.8232"stopColor="#DDD"/>
     </linearGradient>
-    <polygon fill="url(#SVGID_2_)" points="30.1,0 0,31.7 0,60 30.1,29.9 60,0 	"/>
+    <polygon fill="url(#SVGID_2_)" points="30.1,0 0,31.7 0,60 30.1,29.9 60,0 "  stroke="none"/>
 </g>
 </svg>
     )
--- src/components/Icons/React/React.js
+++ src/components/Icons/React/React.js
@@ -2,8 +2,8 @@ const React = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348">
             <title>React Logo</title>
-            <circle cx="0" cy="0" r="2.05" fill="#ffffff"/>
-            <g stroke="#ffffff" strokeWidth="1" fill="none">
+            <circle cx="0" cy="0" r="2.05"/>
+            <g strokeWidth="1" stroke="var(--basic-light)" fill="none">
                 <ellipse rx="11" ry="4.2"/>
                 <ellipse rx="11" ry="4.2" transform="rotate(60)"/>
                 <ellipse rx="11" ry="4.2" transform="rotate(120)"/>
